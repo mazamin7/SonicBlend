18a1a894180a59469987cd38fd5c20e1a65e99a6
Gerardo Cicalese <92589816+mazamin7@users.noreply.github.com>
2023-03-23 15:34:41 +0100

    Removed leftovers

--- basic_functions_implementation/testers/gen_lpc_spec_envs_caller2.m
+++ basic_functions_implementation/testers/gen_lpc_spec_envs_caller2.m
@@ -26,23 +26,11 @@ freq_spec = (-(NFFT/2):(NFFT/2)-1)*fs/NFFT;
 
 % Calculate filter frequency response from "gen_lpc_spec_envs"
 lpc_spec_envs = gen_lpc_spec_envs(xw, p, NFFT);
-lpc_freq_resp1 = lpc_spec_envs(:,19);
-
-% Calculate filter frequency response from "gen_lpc_filter_coeffs"
-lpc_filter_coeffs = gen_lpc_filter_coefs(xw, p);
-coefs = lpc_filter_coeffs(:,19);
-
-% Compute frequency response of LPC filter
-[h, w] = freqz(1, coefs, NFFT, 'whole');
-
-% Compute spectral envelope
-lpc_freq_resp2 = abs(h);
+lpc_freq_resp = lpc_spec_envs(:,19);
 
 % x_frame_stft2 = fft(x_frame, NFFT);
-y1_stft = x_frame_stft .* lpc_freq_resp1;
-y1 = ifft(y1_stft, NFFT);
-
-y2 = filter(1, coefs, x_frame);
+y_stft = x_frame_stft .* lpc_freq_resp;
+y = ifft(y_stft, NFFT);
 
 t = 1:length(x_frame);
 figure;
@@ -59,34 +47,20 @@ ylabel('Magnitude');
 
 % Plot spectral envelope
 figure;
-subplot(211);
-plot(freq_spec, lpc_freq_resp1);
-hold on;
-subplot(212);
-plot(freq_spec, lpc_freq_resp2);
-sgtitle('Spectral Envelope');
+plot(freq_spec, lpc_freq_resp);
+title('Spectral Envelope');
 xlabel('Frequency (rad/sample)');
 ylabel('Magnitude');
 
-t = 1:length(y1);
 figure;
-subplot(211);
-plot(freq_spec, abs(y1_stft));
-hold on;
-subplot(212);
-plot(freq_spec, abs(fft(y2, NFFT)));
-sgtitle('Filtered freq domain signals');
+plot(freq_spec, abs(y_stft));
+title('Filtered freq domain signal');
 xlabel('Time (samples)');
 ylabel('Magnitude');
 
-t = 1:length(y1);
 figure;
-subplot(211);
-plot(t, y1);
-hold on;
-subplot(212);
-plot(t, y2);
-sgtitle('Filtered time domain signals');
+plot(t, y);
+title('Filtered time domain signal');
 xlabel('Time (samples)');
 ylabel('Magnitude');
 
