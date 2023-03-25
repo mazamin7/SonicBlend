7ef14adb8c3294b7d794737abb72a11f389a8744
Gerardo Cicalese <92589816+mazamin7@users.noreply.github.com>
2023-03-25 18:01:50 +0100

    Added test wiener

---
+++ implementation matlab/get_lpc_w_o_tester.m
@@ -0,0 +1,33 @@
+clear all, close all, clc;
+
+addpath library
+
+% Generate random input signal
+x = rand(1,randi([100,200]));
+M = randi([10,20]);
+
+% Calculate lpc using "lpc"
+lpc1 = lpc(x, M);
+lpc1 = -lpc1(2:end)';
+
+% Calculate lpc using custom function (matrix inversion)
+lpc2 = get_lpc_w_o(x, M);
+
+% Calculate lpc using custom function (gradient descent)
+lpc3 = get_lpc_w_o_gd(x, M, 1e3);
+
+% Compare results
+tolerance = 0.01; % 1% tolerance
+if all(abs(lpc1 - lpc2) <= tolerance*abs(lpc1) & sign(lpc1)==sign(lpc2))
+    disp('PASS1')
+else
+    disp('FAIL1')
+end
+
+% Compare results
+tolerance = 0.01; % 1% tolerance
+if all(abs(lpc1 - lpc3) <= tolerance*abs(lpc1) & sign(lpc1)==sign(lpc3))
+    disp('PASS2')
+else
+    disp('FAIL2')
+end
