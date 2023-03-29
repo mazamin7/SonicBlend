89e9e995e42dba9044861939271a79a349cd13c2
Gerardo Cicalese <92589816+mazamin7@users.noreply.github.com>
2023-03-29 11:24:26 +0200

    Further improvements

--- implementation matlab/lpc_test/get_lpc_w_o_gd_visualizer.m
+++ implementation matlab/lpc_test/get_lpc_w_o_gd_visualizer.m
@@ -2,25 +2,16 @@ clear all, close all, clc;
 
 addpath ..\library\
 
-x = rand(1,randi([100,200]));
-M = 2;
 num_iter = 30;
 
-N = length(x);
+% ========== Arguments gd ==========
 
-% calculate autocorrelation
-rx = xcorr(x, M, 'biased');
-rx = N * rx(M+1:end)';
+x = rand(1,randi([100,200]));
+M = 2;
+error_tolerance = 0;
+max_num_iter = num_iter;
 
-R = toeplitz(rx(1:M+1));
-
-eigs_R = eig(R(2:end,2:end));
-factor = 0.3; % with higher values it starts to oscillate
-
-lambda_max = max(eigs_R);
-mu_max = 2/lambda_max;
-
-mu = factor * mu_max;
+% ========== Function gd ==========
 
 % compute optimal solution using "lpc"
 w_o_opt = lpc(x, M);
@@ -32,16 +23,33 @@ J_partial = zeros(num_iter,1);
 
 J_min = sum(x.^2);
 
+N = length(x);
+
+% calculate autocorrelation
+rx = xcorr(x, M, 'biased');
+rx = N * rx(M+1:end)'; % biased autocorrelation
+
+R = toeplitz(rx(1:M+1)); % toeplitz matrix of the autocorrelation
+
+eigs_R = eig(R(2:end,2:end)); % eigenvalues of R matrix (excluding the first row and column)
+factor = 0.3;
+mu_max = 2/max(eigs_R); % maximum value of mu for gradient descent
+mu = factor * mu_max; % learning rate for gradient descent
+
 % initialize coefficients to random values between -1 and 1
 w_o = 2*rand(1,M)'-1;
 
+grad = 1;
+num_iter = 0;
 % perform gradient descent
-for i = 1:num_iter % modify to do this until sufficient convergence is reached
+while (sum(abs(grad)) > error_tolerance) && (num_iter < max_num_iter)
+    % modify to do this until sufficient convergence is reached
     % update coefficients
-    w_o = w_o + mu * (rx(2:end) - R(2:end,2:end) * w_o);
-    w_o_partial(i,:) = w_o;
-    J_partial(i) = J_min + (w_o - w_o_opt)' * R(2:end,2:end) * (w_o - w_o_opt);
-    % e(i) = sum((w_o-w_o_opt).^2);
+    grad = (rx(2:end) - R(2:end,2:end) * w_o);
+    w_o = w_o + mu*grad;
+    w_o_partial(num_iter+1,:) = w_o;
+    J_partial(num_iter+1) = J_min + (w_o - w_o_opt)' * R(2:end,2:end) * (w_o - w_o_opt);
+    num_iter = num_iter + 1;
 end
 
 w1 = w_o_partial(:,1);
@@ -60,3 +68,6 @@ legend('Iteration solutions', 'Optimal solution');
 
 figure;
 plot(1:num_iter,J_partial);
+title('Error vs iteration')
+xlabel('iteration')
+ylabel('J')
--- implementation matlab/lpc_test/get_lpc_w_o_tester.m
+++ implementation matlab/lpc_test/get_lpc_w_o_tester.m
@@ -15,28 +15,28 @@ tic;
 lpc2 = get_lpc_w_o(x, M);
 elapsedTime1 = toc;
 
-disp(['Elapsed time matrix inversion: ' num2str(elapsedTime1) ' seconds']);
+disp(['Matrix inversion method - Elapsed time: ' num2str(elapsedTime1) ' seconds']);
+
+% Compare results
+tolerance = 0.01; % 1% tolerance
+if all(abs(lpc1 - lpc2) <= tolerance*abs(lpc1) & sign(lpc1)==sign(lpc2))
+    disp('Matrix inversion method - PASS')
+else
+    disp('Matrix inversion method - FAIL')
+end
 
 % Calculate lpc using custom function (gradient descent)
 tic;
 [lpc3,num_iter] = get_lpc_w_o_gd(x, M, 1e-4, 1e4);
 elapsedTime2 = toc;
 
-disp(['Elapsed time gradient descent: ' num2str(elapsedTime2) ' seconds']);
-disp(['Iterations gradient descent: ' num2str(num_iter)]);
-
-% Compare results
-tolerance = 0.01; % 1% tolerance
-if all(abs(lpc1 - lpc2) <= tolerance*abs(lpc1) & sign(lpc1)==sign(lpc2))
-    disp('PASS1')
-else
-    disp('FAIL1')
-end
+disp(['Gradient descent method - Elapsed time: ' num2str(elapsedTime2) ' seconds']);
+disp(['Gradient descent method - Iterations: ' num2str(num_iter)]);
 
 % Compare results
 tolerance = 0.01; % 1% tolerance
 if all(abs(lpc1 - lpc3) <= tolerance*abs(lpc1) & sign(lpc1)==sign(lpc3))
-    disp('PASS2')
+    disp('Gradient descent method - PASS')
 else
-    disp('FAIL2')
+    disp('Gradient descent method - FAIL')
 end
