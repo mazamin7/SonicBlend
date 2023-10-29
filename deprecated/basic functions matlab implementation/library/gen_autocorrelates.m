78ecb185a4e79047712bebc91dd5b32701864e00
Gerardo Cicalese <92589816+mazamin7@users.noreply.github.com>
2023-10-29 17:02:57 +0100

    Cleanup

--- deprecated/basic functions matlab implementation/library/gen_autocorrelates.m
+++
@@ -1,8 +0,0 @@
-function [rx] = gen_autocorrelates(x, M)
-    % returns [r_0, r_1, ..., r_M]
-    rx = zeros(M+1,1);
-    for i = 1:M+1
-        rx(i) = x(1:end-i+1)*x(i:end)';
-    end
-    rx = rx';
-end
--- deprecated/basic functions matlab implementation/library/gen_toeplitz.m
+++
@@ -1,8 +0,0 @@
-function [toeplitz] = gen_toeplitz(rx, M)
-    toeplitz = zeros(M);
-    for i = 1:M
-        for j = 1:M
-            toeplitz(i, j) = rx(abs(i-j)+1);
-        end
-    end
-end
